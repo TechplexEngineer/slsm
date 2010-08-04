bb7f37c3b6605e67fbe145a0ca2830505960959c
Techplex.Engineer <Techplex.Engineer@d547794b-2e44-c17f-77fe-e19886bc6b27>
2010-08-04 15:46:30 +0000

    jQuery image popuo and append div

--- OTMS/tests/jQuery-thumbs/index.php
+++ OTMS/tests/jQuery-thumbs/index.php
@@ -12,19 +12,24 @@
                 //Larger thumbnail preview
 
                 $("div.thumb .imgc").hover(function() {
+                    $(this).append('<div id="curr"style="display:none;position:relative;left:-52px;margin-top:10px;border:0px solid black;width:256px;float:left;text-align:center;">HELLO</div>');
                     $(this).css({'z-index' : '10'}); /*Add a higher z-index value so this image stays on top*/
                     $(this).find('img').addClass("hover").stop() /* Add class of "hover", then stop animation queue buildup*/
-                    .animate({
+                   
+                   .animate({
                         marginTop: '-110px', /* The next 4 lines will vertically align this image */
                         marginLeft: '-110px',
                         top: '50%',
                         left: '50%',
-                        width: '200px', /* Set new width */
-                        height: '200px', /* Set new height */
+                        width: '256px', /* Set new width */
+                        height: '192px', /* Set new height */
                         padding: '20px'
                     }, 200); /* this value of "200" is the speed of how fast/slow this hover animates */
-
+                    $(this).find('div').fadeIn();
+                 
                 } , function() {
+                    console.log("junk");
+                    console.log($(this).find("img").attr("src"));
                     $(this).css({'z-index' : '0'}); /* Set z-index back to 0 */
                     $(this).find('img').removeClass("hover").stop()  /* Remove the "hover" class , then stop animation queue buildup*/
                     .animate({
@@ -32,17 +37,27 @@
                         marginLeft: '0',
                         top: '0',
                         left: '0',
-                        width: '100px', /* Set width back to default */
-                        height: '100px', /* Set height back to default */
+                        width: '60px', /* Set width back to default */
+                        height: '45px', /* Set height back to default */
                         padding: '5px'
                     }, 400);
+                    $("#curr").remove();
+                    //$(this).attr({ src: uuid });
                 });
 
                 //Swap Image on Click
                 $("div.thumb .imgc a").click(function() {
 
-                    var mainImage = $(this).attr("href"); //Find Image Name
-                    $("#main_view img").attr({ src: mainImage });
+                    var mainImage = $(this).attr("href"); //get the name from the href of the image clicked
+                    $("#main_view img").attr({ src: mainImage });//change the src of id main_view to the gotten
+                    var uuid = mainImage.substring(32,68);
+                    console.log(uuid);
+                    $("#main_view a").attr({ href: uuid });
+                    return false;
+                });
+                $("#main_view a").click(function() {
+
+                    alert();
                     return false;
                 });
 
@@ -53,33 +68,17 @@
     <body>
         <div class="container">
             <div class="thumb">
-                <div class="imgc"><a href="main_image1.jpg"><img src="thumb1.jpg" alt="" /></a></div>
-                <div class="imgc"><a href="main_image1.jpg"><img src="thumb1.jpg" alt="" /></a></div>
-                <div class="imgc"><a href="main_image1.jpg"><img src="thumb1.jpg" alt="" /></a></div>
-                <div class="imgc"><a href="main_image1.jpg"><img src="thumb1.jpg" alt="" /></a></div>
-                <div class="imgc"><a href="main_image1.jpg"><img src="thumb1.jpg" alt="" /></a></div>
-                <div class="imgc"><a href="main_image1.jpg"><img src="thumb1.jpg" alt="" /></a></div>
-                <div class="imgc"><a href="main_image1.jpg"><img src="thumb1.jpg" alt="" /></a></div>
-                <div class="imgc"><a href="main_image1.jpg"><img src="thumb1.jpg" alt="" /></a></div>
-                <div class="imgc"><a href="main_image1.jpg"><img src="thumb1.jpg" alt="" /></a></div>
-                <div class="imgc"><a href="main_image1.jpg"><img src="thumb1.jpg" alt="" /></a></div>
-                <div class="imgc"><a href="main_image1.jpg"><img src="thumb1.jpg" alt="" /></a></div>
-                <div class="imgc"><a href="main_image1.jpg"><img src="thumb1.jpg" alt="" /></a></div>
-                <div class="imgc"><a href="main_image1.jpg"><img src="thumb1.jpg" alt="" /></a></div>
+                <div class="imgc">
+                    <a href="http://secondlife.com/app/image/d14639ac-8a57-ba75-e325-7fbb20e35225/1"><img src="http://secondlife.com/app/image/d14639ac-8a57-ba75-e325-7fbb20e35225/3" alt="" /></a>
+                </div>
+                
 
 
-                </div>
-                <div id="main_view">
-                    <a href="http://www.DesignBombs.com" title="Design Bombs - Web Gallery" target="_blank"><img src="main_image1.jpg" alt="" /></a><br />
-            <!--	<small style="float: right; color: #999;">Tutorial by <a style="color: #777;" href="http://www.SohTanaka.com">Soh Tanaka</a></small>-->
-                </div>
+            </div>
+            <div id="main_view">
+                <a href="http://www.DesignBombs.com" title="Design Bombs - Web Gallery" target="_blank"><img src="main_image1.jpg" alt="" /></a><br />
             </div>
         </div>
     </body>
 </html> 
-<?php
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+
